f955d35e0d0fa0b0e6343e4455ccac00ef00de28
Mattias Johansson <mattias42@gmail.com>
2019-10-27 09:57:00 +0100

    structure on the plume spectrum selector

--- include/SpectralEvaluation/Evaluation/PlumeSpectrumSelector.h
+++ include/SpectralEvaluation/Evaluation/PlumeSpectrumSelector.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 
 class CPlumeInScanProperty;
 
@@ -6,21 +7,39 @@ namespace Evaluation
 {
     class BasicScanEvaluationResult;
 
-    struct PlumeSpectrumSelectionSettings
-    {
-        int minNumberOfSpectraInPlume = 5;
-        int minNumberOfReferenceSpectra = 5;
-    };
-
-
     // This is a helper class for selecting 'in plume' and 'out of plume' 
     //  (based on some basic selection criteria) spectra with 
     //  the intention of creating reference spectra for performing e.g. 
     //  ratio evaluations or detailed spectral analysis of compounds in the plume.
     class PlumeSpectrumSelector
     {
+        struct PlumeSpectrumSelectionSettings
+        {
+            // The minimum number of spectra which needs to be selected in the plume for the ratio calculation to be successful.
+            int minNumberOfSpectraInPlume = 7;
+
+            // The minimum (SO2) column for the selected spectra in the plume.
+            double minInPlumeColumn = 1e17;
+
+            // The minimum number of spectra which needs to be averaged outside of the plume for the calculation to be successful.
+            int minNumberOfReferenceSpectra = 7;
+        };
+
+        // Checks the provided evaluated scan using default settings 
+        //  and returns the indices of the spectra which can 
+        //  be saved as in-plume and out-of-plume spectra.
+        // If this check fails, then the two vectors are empty.
+        // TODO: This also needs a reference to the ScanFile such that it can check the intensity of the spectra in various regions!
+        void SelectSpectra(const BasicScanEvaluationResult& scanResult, const CPlumeInScanProperty& properties, int mainSpecieIndex, std::vector<int>& referenceSpectra, std::vector<int>& inPlumeSpectra);
+
+    private:
+
         static bool IsSuitableScanForRatioEvaluation(const PlumeSpectrumSelectionSettings& settings, const BasicScanEvaluationResult& scanResult, const CPlumeInScanProperty& properties);
 
+        static void SelectSpectra(const PlumeSpectrumSelectionSettings& settings, const BasicScanEvaluationResult& scanResult, const CPlumeInScanProperty& properties, int mainSpecieIndex, std::vector<int>& referenceSpectra, std::vector<int>& inPlumeSpectra);
+
+        // Calculates the average column value of the given specie in the index [startIdx, endIdx[
+        static double AverageColumnValue(const BasicScanEvaluationResult& scanResult, int specieIndex, size_t startIdx, size_t endIdx);
 
     };
 }
--- src/Evaluation/PlumeSpectrumSelector.cpp
+++ src/Evaluation/PlumeSpectrumSelector.cpp
@@ -1,8 +1,85 @@
+#include <numeric>
+
 #include <SpectralEvaluation/Evaluation/PlumeSpectrumSelector.h>
+
 #include <SpectralEvaluation/Evaluation/BasicScanEvaluationResult.h>
 #include <SpectralEvaluation/Flux/PlumeInScanProperty.h>
 
 using namespace Evaluation;
+
+void PlumeSpectrumSelector::SelectSpectra(
+    const BasicScanEvaluationResult& scanResult,
+    const CPlumeInScanProperty& properties,
+    int mainSpecieIndex,
+    std::vector<int>& referenceSpectra,
+    std::vector<int>& inPlumeSpectra)
+{
+    PlumeSpectrumSelectionSettings defaultSettings;
+    return SelectSpectra(defaultSettings, scanResult, properties, mainSpecieIndex, referenceSpectra, inPlumeSpectra);
+}
+
+
+void PlumeSpectrumSelector::SelectSpectra(
+    const PlumeSpectrumSelectionSettings& settings,
+    const BasicScanEvaluationResult& scanResult,
+    const CPlumeInScanProperty& properties,
+    int mainSpecieIndex,
+    std::vector<int>& referenceSpectra,
+    std::vector<int>& inPlumeSpectra)
+{
+    referenceSpectra.clear();
+    inPlumeSpectra.clear();
+
+    const size_t referenceRegionWidth = 10U;
+
+    if (scanResult.m_spec.size() <= referenceRegionWidth)
+    {
+        return; // Cannot retrieve a region, too few spectra...
+    }
+
+    // Step 1, find the in-plume region.
+    for (size_t idx = 0; idx < scanResult.m_spec.size(); ++idx)
+    {
+        if (scanResult.m_specInfo[idx].m_scanAngle >= properties.plumeEdgeLow &&
+            scanResult.m_specInfo[idx].m_scanAngle <= properties.plumeEdgeHigh &&
+            scanResult.m_spec[idx].m_referenceResult[mainSpecieIndex].m_column >= settings.minInPlumeColumn)
+        {
+            inPlumeSpectra.push_back((int)idx);
+        }
+    }
+
+    // Step 2, find the reference region (10 adjacent spectra with lowest avg column value)
+    size_t startIdxOfReferenceRegion = 0U;
+    double lowestMeanColumnValue = std::numeric_limits<double>::max();
+    for (size_t startIdxCandidate = 0U; startIdxCandidate < scanResult.m_spec.size() - referenceRegionWidth; ++startIdxCandidate)
+    {
+        const double meanColumnValue = AverageColumnValue(scanResult, mainSpecieIndex, startIdxCandidate, startIdxCandidate + referenceRegionWidth);
+        if (meanColumnValue < lowestMeanColumnValue)
+        {
+            lowestMeanColumnValue = meanColumnValue;
+            startIdxOfReferenceRegion = startIdxCandidate;
+        }
+    }
+
+    referenceSpectra = std::vector<int>(referenceRegionWidth);
+    std::iota(begin(referenceSpectra), end(referenceSpectra), (int)startIdxOfReferenceRegion);
+
+    return;
+}
+
+double PlumeSpectrumSelector::AverageColumnValue(
+    const BasicScanEvaluationResult& scanResult,
+    int specieIndex,
+    size_t startIdx,
+    size_t endIdx)
+{
+    double sum = 0.0;
+    for (size_t ii = startIdx; ii < endIdx; ++ii)
+    {
+        sum += scanResult.m_spec[ii].m_referenceResult[specieIndex].m_column;
+    }
+    return sum / (double)(endIdx - startIdx);
+}
 
 bool PlumeSpectrumSelector::IsSuitableScanForRatioEvaluation(
     const PlumeSpectrumSelectionSettings& settings,
