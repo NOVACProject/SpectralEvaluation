6f6068359a82a621cd4b3f938d2d5dacbe778071
Mattias Johansson <mattias42@gmail.com>
2019-11-16 14:59:57 +0100

    adde trilinear interpolation as well

--- include/SpectralEvaluation/Interpolation.h
+++ include/SpectralEvaluation/Interpolation.h
@@ -7,10 +7,21 @@
 #include <vector>
 
 /** Finds the value of the provided vector at the given index by linear interpolation.
-    @returns the interpolated value, or NAN if the index is negative or larger than (values.size() - 1). */
-double LinearInterpolation(const std::vector<float>& values, double index);
+    @returns true if the interpolated could be found.
+    @returns false if the given index is negative or larger than (values.size() - 1). */
+bool LinearInterpolation(const std::vector<float>& values, double index, double& result);
+
+/** Performs a tri-linear interpolation to find the value with the index {index[0], index[1], index[2]}
+    in the three dimensional data matrix. The 'values' vector is a flattened three dimensional data
+    matrix consisting of eight values, one value for each corner of the unit-cube. 
+    This assumes that values is a cube with eight values and index is an index vector with three values
+        which are all in the range [0, 1].
+    @return true if the interpolation could be performed successfully.
+    @return false if any of the above assumptions are not fulfilled. */
+bool TriLinearInterpolation(const std::vector<double>& values, const std::vector<double>& index, double& result);
+bool TriLinearInterpolation(const std::vector<double>& values, const std::vector<double>& index, double& result, double& estimatedVariation);
 
 /** Finds the provided value in the given vector of values. 
     The vector of values is assumed to be sorted in either ascending or descending order.
     @return the index or NAN if the value could not be found. */
-double GetFractionalIndex(const std::vector<float>& values, float valueToFind);
+double GetFractionalIndex(const std::vector<float>& values, double valueToFind);
--- src/Interpolation.cpp
+++ src/Interpolation.cpp
@@ -1,21 +1,56 @@
 #include <SpectralEvaluation/Interpolation.h>
 #include <cmath>
 
-double LinearInterpolation(const std::vector<float>& values, double index)
+bool LinearInterpolation(const std::vector<float>& values, double index, double& result)
 {
     const size_t ii = (size_t)std::floor(index);
 
     if (ii >= values.size() - 2)
     {
-        return NAN; // not found.
+        return false; // not found.
     }
 
     const double alpha = index - (double)ii;
-
-    return values[ii] * (1.0 - alpha) + values[ii + 1] * alpha;
+    result = values[ii] * (1.0 - alpha) + values[ii + 1] * alpha;
+    return true;
 }
 
-double GetFractionalIndex(const std::vector<float>& values, float valueToFind)
+bool TriLinearInterpolation(const std::vector<double>& values, const std::vector<double>& index, double& result)
+{
+    double ignoredValue;
+    return TriLinearInterpolation(values, index, result, ignoredValue);
+}
+
+bool TriLinearInterpolation(const std::vector<double>& values, const std::vector<double>& index, double& result, double& estimatedVariation)
+{
+    if (values.size() != 8 || 
+        index.size() != 3 ||
+        index[0] < 0.0 || index[0] > 1.0 ||
+        index[1] < 0.0 || index[1] > 1.0 ||
+        index[2] < 0.0 || index[2] > 1.0)
+    {
+        return false;
+    }
+
+    const double idxX = index[0];
+    const double idxY = index[1];
+    const double idxZ = index[2];
+
+    const double c00 = values[0] * (1.0 - idxZ) + values[1] * idxZ;
+    const double c01 = values[2] * (1.0 - idxZ) + values[3] * idxZ;
+    const double c10 = values[4] * (1.0 - idxZ) + values[5] * idxZ;
+    const double c11 = values[6] * (1.0 - idxZ) + values[7] * idxZ;
+
+    const double c0 = c00 * (1.0 - idxY) + c01 * idxY;
+    const double c1 = c10 * (1.0 - idxY) + c11 * idxY;
+
+    result = c0 * (1.0 - idxX) + c1 * idxX;
+    estimatedVariation = c1 - c0; // TODO: this isn't really correct is it?
+
+    return true;
+}
+
+double GetFractionalIndex(const std::vector<float>& values, double valueToFind)
 {
     for (size_t ii = 1; ii < values.size(); ++ii)
     {
