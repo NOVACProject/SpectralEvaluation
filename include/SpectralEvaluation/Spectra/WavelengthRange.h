b36cd9b518253a76d9e7264033f8751c69118226
Mattias Johansson <mattias42@gmail.com>
2020-03-15 14:35:07 +0100

    Added a separate class to indicate a wavelength range

---
+++ include/SpectralEvaluation/Spectra/WavelengthRange.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Evaluation
+{
+
+struct WavelengthRange
+{
+	WavelengthRange(double from, double to)
+	{
+		this->low = (from < to) ? from : to;
+		this->high = (from < to) ? to : from;
+	}
+	double low = 0.0;
+	double high = 0.0;
+};
+
+}
