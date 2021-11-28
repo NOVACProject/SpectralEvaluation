8f506275e6e315a8e533d4f9d0c193df9315856c
Mattias Johansson <mattias42@gmail.com>
2021-11-28 07:45:34 +0100

    Code style + added copy-constructor to InstrumentCalibration

--- include/SpectralEvaluation/Calibration/InstrumentCalibration.h
+++ include/SpectralEvaluation/Calibration/InstrumentCalibration.h
@@ -4,46 +4,49 @@
 
 namespace novac
 {
-class ParametricInstrumentLineShape;
+    class ParametricInstrumentLineShape;
 
-/** This class is an attempt to represent everything there is to know about
-    the calibration of an instrument into one place.
-    The instrument line shape is considered to be not wavelength dependent. */
-class InstrumentCalibration
-{
-public:
-    InstrumentCalibration() { };
+    /** This class is an attempt to represent everything there is to know about
+        the calibration of an instrument into one place.
+        The instrument line shape is considered to be not wavelength dependent. */
+    class InstrumentCalibration
+    {
+    public:
+        InstrumentCalibration() { };
 
-    ~InstrumentCalibration();
+        ~InstrumentCalibration();
 
-    void Clear();
+        void Clear();
 
-    // This object is not copyable due to the contained pointer.
-    //  This could be implemented in the future if necessary
-    InstrumentCalibration(const InstrumentCalibration&) = delete;
-    InstrumentCalibration& operator=(const InstrumentCalibration&) = delete;
+        // Explicit copy operator. Makes sure that the pointer is copied properly.
+        InstrumentCalibration(const InstrumentCalibration&);
+        InstrumentCalibration& operator=(const InstrumentCalibration&);
 
-    /** The coefficients of the pixel to wavelength calibration.
-        With the zero:th order coefficient first. */
-    std::vector<double> pixelToWavelengthPolynomial;
+        // The object is however moveable.
+        InstrumentCalibration(InstrumentCalibration&&);
+        InstrumentCalibration& operator=(InstrumentCalibration&&);
 
-    /** The mapping from pixels to wavelength.
-        Length equals number of pixels on detector. */
-    std::vector<double> pixelToWavelengthMapping;
+        /** The coefficients of the pixel to wavelength calibration.
+            With the zero:th order coefficient first. */
+        std::vector<double> pixelToWavelengthPolynomial;
 
-    /** A sampling of the instrument line shape.
-        These values should be normalized, i.e. fall in the range [0, 1] */
-    std::vector<double> instrumentLineShape;
+        /** The mapping from pixels to wavelength.
+            Length equals number of pixels on detector. */
+        std::vector<double> pixelToWavelengthMapping;
 
-    /** A sampling grid for the instrument line shape.
-        Must have equal length as 'instrumentLineShape' */
-    std::vector<double> instrumentLineShapeGrid;
+        /** A sampling of the instrument line shape.
+            These values should be normalized, i.e. fall in the range [0, 1] */
+        std::vector<double> instrumentLineShape;
 
-    /** The wavelength where the current instrument line shape is representative. */
-    double instrumentLineShapeCenter = 0.0;
+        /** A sampling grid for the instrument line shape.
+            Must have equal length as 'instrumentLineShape' */
+        std::vector<double> instrumentLineShapeGrid;
 
-    /** A parametrization of the instrument line shape */
-    ParametricInstrumentLineShape* instrumentLineShapeParameter = nullptr;
-};
+        /** The wavelength where the current instrument line shape is representative. */
+        double instrumentLineShapeCenter = 0.0;
+
+        /** A parametrization of the instrument line shape */
+        ParametricInstrumentLineShape* instrumentLineShapeParameter = nullptr;
+    };
 
 }
--- src/Calibration/StandardCrossSectionSetup.cpp
+++ src/Calibration/StandardCrossSectionSetup.cpp
@@ -10,145 +10,145 @@
 namespace novac
 {
 
-StandardCrossSectionSetup::StandardCrossSectionSetup(const std::string& directory)
-{
-    // check if there is a set of standard references in the expected format in the provided directory.
-
-    try
+    StandardCrossSectionSetup::StandardCrossSectionSetup(const std::string& directory)
     {
-        StandardReference currentReference;
+        // check if there is a set of standard references in the expected format in the provided directory.
 
-        std::string xmlFileLocation = directory;
-        if (xmlFileLocation.back() != '/' && xmlFileLocation.back() != '\\')
+        try
         {
-            xmlFileLocation.append("/");
+            StandardReference currentReference;
+
+            std::string xmlFileLocation = directory;
+            if (xmlFileLocation.size() > 0 && xmlFileLocation.back() != '/' && xmlFileLocation.back() != '\\')
+            {
+                xmlFileLocation.append("/");
+            }
+            xmlFileLocation.append("StandardCrossSections/");
+
+            std::string xmlFileName = xmlFileLocation;
+            xmlFileName.append("StandardCrossSections.xml");
+
+            // Super basic xml parsing, without using any additional libraries.
+            std::ifstream file(xmlFileName, std::ios::in);
+            std::string line;
+            while (std::getline(file, line))
+            {
+                if (line.find("/CrossSection") != std::string::npos)
+                {
+                    // Add the currently read reference to the end, if it points to a valid location.
+                    if (IsExistingFile(currentReference.fileName))
+                    {
+                        m_standardReferences.push_back(currentReference);
+                    }
+                }
+                else if (line.find("CrossSection") != std::string::npos)
+                {
+                    // Start the reading over with a new reference
+                    currentReference = StandardReference();
+                }
+                else if (line.find("/FraunhoferSpectrum") != std::string::npos)
+                {
+                    // Add the currently read reference as the Fraunhofer reference to use, if it points to a valid location.
+                    if (IsExistingFile(currentReference.fileName))
+                    {
+                        m_fraunhoferReference.fileName = currentReference.fileName;
+                        m_fraunhoferReference.isVacuum = currentReference.isVacuum;
+                    }
+                }
+                else if (line.find("FraunhoferSpectrum") != std::string::npos)
+                {
+                    // Start the reading over with a new reference
+                    currentReference = StandardReference();
+                }
+                else if (line.find("File") != std::string::npos)
+                {
+                    std::string fileName = ParseXmlString("File", line);
+                    if (fileName.size() > 0)
+                    {
+                        currentReference.fileName = xmlFileLocation;
+                        currentReference.fileName.append(fileName);
+                    }
+                }
+                else if (line.find("Name") != std::string::npos)
+                {
+                    currentReference.specieName = ParseXmlString("Name", line);
+                }
+                else if (line.find("Medium") != std::string::npos)
+                {
+                    std::string mediumName = ParseXmlString("Medium", line);
+
+                    if (mediumName.length() > 0 && EqualsIgnoringCase(mediumName.c_str(), "air", 3))
+                    {
+                        currentReference.isVacuum = false;
+                    }
+                    else
+                    {
+                        currentReference.isVacuum = true;
+                    }
+                }
+            }
         }
-        xmlFileLocation.append("StandardCrossSections/");
-
-        std::string xmlFileName = xmlFileLocation;
-        xmlFileName.append("StandardCrossSections.xml");
-
-        // Super basic xml parsing, without using any additional libraries.
-        std::ifstream file(xmlFileName, std::ios::in);
-        std::string line;
-        while (std::getline(file, line))
+        catch (std::exception&)
         {
-            if (line.find("/CrossSection") != std::string::npos)
-            {
-                // Add the currently read reference to the end, if it points to a valid location.
-                if (IsExistingFile(currentReference.fileName))
-                {
-                    m_standardReferences.push_back(currentReference);
-                }
-            }
-            else if (line.find("CrossSection") != std::string::npos)
-            {
-                // Start the reading over with a new reference
-                currentReference = StandardReference();
-            }
-            else if (line.find("/FraunhoferSpectrum") != std::string::npos)
-            {
-                // Add the currently read reference as the Fraunhofer reference to use, if it points to a valid location.
-                if (IsExistingFile(currentReference.fileName))
-                {
-                    m_fraunhoferReference.fileName = currentReference.fileName;
-                    m_fraunhoferReference.isVacuum = currentReference.isVacuum;
-                }
-            }
-            else if (line.find("FraunhoferSpectrum") != std::string::npos)
-            {
-                // Start the reading over with a new reference
-                currentReference = StandardReference();
-            }
-            else if (line.find("File") != std::string::npos)
-            {
-                std::string fileName = ParseXmlString("File", line);
-                if (fileName.size() > 0)
-                {
-                    currentReference.fileName = xmlFileLocation;
-                    currentReference.fileName.append(fileName);
-                }
-            }
-            else if (line.find("Name") != std::string::npos)
-            {
-                currentReference.specieName = ParseXmlString("Name", line);
-            }
-            else if (line.find("Medium") != std::string::npos)
-            {
-                std::string mediumName = ParseXmlString("Medium", line);
-
-                if (mediumName.length() > 0 && EqualsIgnoringCase(mediumName.c_str(), "air", 3))
-                {
-                    currentReference.isVacuum = false;
-                }
-                else
-                {
-                    currentReference.isVacuum = true;
-                }
-            }
         }
     }
-    catch (std::exception&)
-    {
-    }
-}
 
-std::vector<std::string> StandardCrossSectionSetup::ListReferences() const
-{
-    std::vector<std::string> result;
-
-    for (const auto& reference : m_standardReferences)
+    std::vector<std::string> StandardCrossSectionSetup::ListReferences() const
     {
-        std::string name = reference.specieName;
-        if (reference.isVacuum)
+        std::vector<std::string> result;
+
+        for (const auto& reference : m_standardReferences)
         {
-            name.append(" (vac)");
+            std::string name = reference.specieName;
+            if (reference.isVacuum)
+            {
+                name.append(" (vac)");
+            }
+            result.push_back(name);
         }
-        result.push_back(name);
+        return result;
     }
-    return result;
-}
 
-void ThrowInvalidIndexException(size_t requestedIndex, size_t vectorSize)
-{
-    std::stringstream message;
-    message << "Invalid index (" << requestedIndex << ") into the list of references, containing " << vectorSize << " elements.";
-    throw std::invalid_argument(message.str());
-}
-
-std::string StandardCrossSectionSetup::ReferenceFileName(size_t index) const
-{
-    if (index >= m_standardReferences.size())
+    void ThrowInvalidIndexException(size_t requestedIndex, size_t vectorSize)
     {
-        ThrowInvalidIndexException(index, m_standardReferences.size());
+        std::stringstream message;
+        message << "Invalid index (" << requestedIndex << ") into the list of references, containing " << vectorSize << " elements.";
+        throw std::invalid_argument(message.str());
     }
 
-    return m_standardReferences[index].fileName;
-}
-
-std::string StandardCrossSectionSetup::ReferenceSpecieName(size_t index) const
-{
-    if (index >= m_standardReferences.size())
+    std::string StandardCrossSectionSetup::ReferenceFileName(size_t index) const
     {
-        ThrowInvalidIndexException(index, m_standardReferences.size());
+        if (index >= m_standardReferences.size())
+        {
+            ThrowInvalidIndexException(index, m_standardReferences.size());
+        }
+
+        return m_standardReferences[index].fileName;
     }
 
-    return m_standardReferences[index].specieName;
-}
-
-bool StandardCrossSectionSetup::IsReferenceInVacuum(size_t index) const
-{
-    if (index >= m_standardReferences.size())
+    std::string StandardCrossSectionSetup::ReferenceSpecieName(size_t index) const
     {
-        ThrowInvalidIndexException(index, m_standardReferences.size());
+        if (index >= m_standardReferences.size())
+        {
+            ThrowInvalidIndexException(index, m_standardReferences.size());
+        }
+
+        return m_standardReferences[index].specieName;
     }
 
-    return m_standardReferences[index].isVacuum;
-}
+    bool StandardCrossSectionSetup::IsReferenceInVacuum(size_t index) const
+    {
+        if (index >= m_standardReferences.size())
+        {
+            ThrowInvalidIndexException(index, m_standardReferences.size());
+        }
 
-std::string StandardCrossSectionSetup::FraunhoferReferenceFileName() const
-{
-    return m_fraunhoferReference.fileName;
-}
+        return m_standardReferences[index].isVacuum;
+    }
+
+    std::string StandardCrossSectionSetup::FraunhoferReferenceFileName() const
+    {
+        return m_fraunhoferReference.fileName;
+    }
 
 }
