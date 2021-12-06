a53c9779469103e7af67e314dfdc2e887c439a73
Mattias Johansson <mattias42@gmail.com>
2021-12-06 07:58:53 +0100

    Moved out the NovacProgramWavelengthCalibrationController to a separate class inside the NovacProgram as the references here caused a problem for MobileDoas.

--- include/SpectralEvaluation/DialogControllers/WavelengthCalibrationController.h
+++ include/SpectralEvaluation/DialogControllers/WavelengthCalibrationController.h
@@ -173,19 +173,4 @@ protected:
 
 };
 
-class NovacProgramWavelengthCalibrationController : public WavelengthCalibrationController
-{
-public:
-    NovacProgramWavelengthCalibrationController()
-        : WavelengthCalibrationController() { }
-
-
-    /** The full path to the spectrum to calibrate (should be a .pak file) */
-    std::string m_inputSpectrumFile;
-
-protected:
-
-    virtual void ReadInput(novac::CSpectrum& measurement) override;
-
-};
 
--- src/DialogControllers/WavelengthCalibrationController.cpp
+++ src/DialogControllers/WavelengthCalibrationController.cpp
@@ -10,7 +10,6 @@
 #include <SpectralEvaluation/Spectra/Spectrum.h>
 #include <SpectralEvaluation/Spectra/SpectrometerModel.h>
 #include <SpectralEvaluation/File/File.h>
-#include <SpectralEvaluation/File/ScanFileHandler.h>
 #include <SpectralEvaluation/Calibration/Correspondence.h>
 #include <SpectralEvaluation/Calibration/InstrumentCalibration.h>
 #include <SpectralEvaluation/Calibration/WavelengthCalibration.h>
@@ -426,28 +425,3 @@ void MobileDoasWavelengthCalibrationController::ReadInput(novac::CSpectrum& meas
     }
 }
 
-void NovacProgramWavelengthCalibrationController::ReadInput(novac::CSpectrum& measuredSpectrum)
-{
-    novac::CScanFileHandler pakFileHandler;
-    if (!pakFileHandler.CheckScanFile(m_inputSpectrumFile))
-    {
-        std::stringstream msg;
-        msg << "Cannot read the provided input spectrum file. Error:  " << pakFileHandler.m_lastError;
-        throw std::invalid_argument(msg.str());
-    }
-
-    if (pakFileHandler.GetSky(measuredSpectrum))
-    {
-        throw std::invalid_argument("Cannot read the provided input spectrum file");
-    }
-    Log("Read measured spectrum: ", m_inputSpectrumFile);
-
-    // subtract the dark-spectrum (if this exists)
-    novac::CSpectrum darkSpectrum;
-    if (!pakFileHandler.GetDark(darkSpectrum))
-    {
-        measuredSpectrum.Sub(darkSpectrum);
-        Log("Read and subtracted dark spectrum");
-    }
-
-}
