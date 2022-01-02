3cbc649078291b50faab353b3519a079fefcce90
Mattias Johansson <mattias42@gmail.com>
2022-01-02 13:56:47 +0100

    bug-fix: the fraunhofer spectrum wasn't regenerated in the last iteration (since it was not useful for the calibration anymore) however it was still useful for the plots in mobileDoas + novacProgram. Not regenerating the spectrum caused a bug where the plot showed an erroneous fraunhofer spectrum.

--- src/Calibration/WavelengthCalibration.cpp
+++ src/Calibration/WavelengthCalibration.cpp
@@ -528,21 +528,16 @@ namespace novac
             // Adjust the selection parameter for the maximum error in the wavelength calibration such that the search space decreases for each iteration
             correspondenceSelectionSettings.maximumPixelDistanceForPossibleCorrespondence = std::max(10, correspondenceSelectionSettings.maximumPixelDistanceForPossibleCorrespondence / 10);
 
-            if (iterationIdx < numberOfIterations - 1)
+            // Adjust the shape of the Fraunhofer spectrum using what we now know:
+            //  We have the sensitivity of the spectrometer (pixel -> intesity) from the calculated envelope
+            //  We have the pixel-to-wavelength mapping of the spectrometer from the ransac calibration
+            // Re-convolve the Fraunhofer spectrum to get it on the new pixel grid and with the new instrument line shape (if updated)
             {
-                // Adjust the shape of the Fraunhofer spectrum using what we now know:
-                //  We have the sensitivity of the spectrometer (pixel -> intesity) from the calculated envelope
-                //  We have the pixel-to-wavelength mapping of the spectrometer from the ransac calibration
-                // Re-convolve the Fraunhofer spectrum to get it on the new pixel grid and with the new instrument line shape (if updated)
-
-                std::cout << " -- Adjusting Fraunhofer Spectrum -- " << std::endl;
-                {
-                    novac::CCrossSectionData& currentEstimateOfInstrumentLineShape = (result.estimatedInstrumentLineShape.GetSize() > 0) ? result.estimatedInstrumentLineShape : settings.initialInstrumentLineShape;
-                    calibrationState.fraunhoferSpectrum = fraunhoferSetup.GetFraunhoferSpectrum(result.pixelToWavelengthMapping, currentEstimateOfInstrumentLineShape);
-                }
-
-                UpdateFraunhoferSpectrumWithApparentSensitivity(ransacResult);
+                novac::CCrossSectionData& currentEstimateOfInstrumentLineShape = (result.estimatedInstrumentLineShape.GetSize() > 0) ? result.estimatedInstrumentLineShape : settings.initialInstrumentLineShape;
+                calibrationState.fraunhoferSpectrum = fraunhoferSetup.GetFraunhoferSpectrum(result.pixelToWavelengthMapping, currentEstimateOfInstrumentLineShape);
             }
+
+            UpdateFraunhoferSpectrumWithApparentSensitivity(ransacResult);
         }
 
         // Normalize the output, such that other programs may use the data directly.
