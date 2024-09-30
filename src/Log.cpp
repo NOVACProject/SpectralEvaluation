74282e95e570bcd430242645503ea494ccbb62a4
Mattias Johansson <mattias42@gmail.com>
2024-09-30 05:52:40 +0200

    spacing

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,20 +1,17 @@
 #include <SpectralEvaluation/Log.h>
 #include <iostream>
 
-using namespace novac;
-
-
 void novac::ConsoleLog::Debug(const std::string& message)
 {
-    std::cout << "[Debug]" << message << std::endl;
+    std::cout << "[Debug] " << message << std::endl;
 }
 
 void novac::ConsoleLog::Information(const std::string& message)
 {
-    std::cout << "[Info]" << message << std::endl;
+    std::cout << "[Info] " << message << std::endl;
 }
 
 void novac::ConsoleLog::Error(const std::string& message)
 {
-    std::cout << "[Error]" << message << std::endl;
+    std::cout << "[Error] " << message << std::endl;
 }
