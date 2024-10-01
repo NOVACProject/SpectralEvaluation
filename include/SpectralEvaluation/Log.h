c67db0b73234fa6e587ccb60f17b55510816052c
Mattias Johansson <mattias42@gmail.com>
2024-10-01 06:58:21 +0200

    Expanded log, resolved some intentation issues

--- include/SpectralEvaluation/Log.h
+++ include/SpectralEvaluation/Log.h
@@ -1,19 +1,35 @@
 #pragma once
 
+#include <vector>
 #include <string>
 
 namespace novac
 {
+
+class LogContext
+{
+public:
+    LogContext() = default;
+
+    ~LogContext() = default;
+
+    std::vector<std::pair<std::string, std::string>> properties;
+
+    LogContext With(std::string name, std::string value);
+};
 
 /** Abstract logger base class. */
 class ILogger
 {
 public:
     virtual void Debug(const std::string& message) = 0;
+    virtual void Debug(const LogContext& c, const std::string& message) = 0;
 
     virtual void Information(const std::string& message) = 0;
+    virtual void Information(const LogContext& c, const std::string& message) = 0;
 
     virtual void Error(const std::string& message) = 0;
+    virtual void Error(const LogContext& c, const std::string& message) = 0;
 };
 
 /** The simplest form of logging, using the console */
@@ -21,15 +37,14 @@ class ConsoleLog : public ILogger
 {
 public:
 
-    // Inherited via ILogger
     virtual void Debug(const std::string& message) override;
+    virtual void Debug(const LogContext& c, const std::string& message) override;
 
     virtual void Information(const std::string& message) override;
+    virtual void Information(const LogContext& c, const std::string& message) override;
 
     virtual void Error(const std::string& message) override;
-
+    virtual void Error(const LogContext& c, const std::string& message) override;
 };
-
-
 
 }
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,17 +1,60 @@
 #include <SpectralEvaluation/Log.h>
 #include <iostream>
 
+
+novac::LogContext novac::LogContext::With(std::string name, std::string value)
+{
+    novac::LogContext c(*this);
+    c.properties.push_back(std::pair<std::string, std::string>(name, value));
+    return c;
+}
+
+static std::ostream& operator << (std::ostream& out, const novac::LogContext& c)
+{
+    for each (const auto & p in c.properties)
+    {
+        out << "[" << p.first << "=" << p.second << "] ";
+    }
+
+    return out;
+}
+
+static void Output(const char* level, std::string message)
+{
+    std::cout << level << message << std::endl;
+}
+
+static void Output(const char* level, const novac::LogContext& c, std::string message)
+{
+    std::cout << level << c << message << std::endl;
+}
+
 void novac::ConsoleLog::Debug(const std::string& message)
 {
-    std::cout << "[Debug] " << message << std::endl;
+    Output("[Debug] ", message);
+}
+
+void novac::ConsoleLog::Debug(const LogContext& c, const std::string& message)
+{
+    Output("[Debug] ", c, message);
 }
 
 void novac::ConsoleLog::Information(const std::string& message)
 {
-    std::cout << "[Info] " << message << std::endl;
+    Output("[Info] ", message);
+}
+
+void novac::ConsoleLog::Information(const LogContext& c, const std::string& message)
+{
+    Output("[Info] ", c, message);
 }
 
 void novac::ConsoleLog::Error(const std::string& message)
 {
-    std::cout << "[Error] " << message << std::endl;
+    Output("[Error] ", message);
+}
+
+void novac::ConsoleLog::Error(const LogContext& c, const std::string& message)
+{
+    Output("[Error] ", c, message);
 }
